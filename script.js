d1a459a88eff64db215bdafc825e75973c671108
Marine <marine.marez@gmail.com>
2022-02-17 15:12:37 +0100

    étape 3.1 exercice

--- script.js
+++ script.js
@@ -1,35 +1,56 @@
+// Déclaration de variable globale : 
+let givenNumber
+
 // Déclartion de la fonction asNumber sans paramètre
 function askNumber()
 {
-	let givenNumber = prompt("Indiquez un nombre entre 0 et 50 : ");	
+	givenNumber = prompt("Indiquez un nombre entre 0 et 50 : ");	
 	console.log(givenNumber);
-	document.body.innerHTML += givenNumber;
 	return givenNumber
 }
 
-// Déclaration de la focntion diIWin
-function didIWin(number)
+/*Déclaration de la focntion didIWin étape 2 
+
+function didIWin(givenNumber)
 {
-	if (number == 22)
+	if (givenNumber == 22)
 	{	
 		let displayWin = "Bravo, vous avez deviné le nombre";
 		console.log (displayWin);
-}
-//	return givenNumber;
-
-	else if (number<22)
+	}
+	else if (givenNumber<22)
 	{
 		let displayMore = "Plus grand";
 		console.log (displayMore)
 	}
-
-	else if (number>22)
+	else if (givenNumber>22)
 	{
 		let displayLess = "Plus petit";
 		console.log(displayLess)
 	}
+} */
+
+// déclarationde la fonction didIwine étape 3 true false
+
+function didIWin(givenNumber)
+{
+	if (givenNumber == 22)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false
+	}
 }
+
+//console.log(didIWin(15));
+
+
+// function gamePlay()
+
 
 let a = askNumber();
 didIWin(a);
-
+document.body.innerHTML += didIWin();
